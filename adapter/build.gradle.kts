b49e6f3c5e826adc59412fe7bbfe133a6ec554c8
gosekose <ksyn1611@naver.com>
2023-10-14 02:39:33 +0900

    [Feat] DPLC-019: Place 정보 제공 Grpc proto 설정

--- adapter/build.gradle.kts
+++ adapter/build.gradle.kts
@@ -1,4 +1,5 @@
 import org.springframework.boot.gradle.tasks.bundling.BootJar
+import com.google.protobuf.gradle.*
 
 val jar: Jar by tasks
 val bootJar: BootJar by tasks
@@ -8,13 +9,12 @@ jar.enabled = true
 
 plugins {
     kotlin("plugin.jpa") version "1.8.22"
-    id("com.ewerk.gradle.plugins.querydsl") version "1.0.10"
+    id("com.google.protobuf") version "0.9.4"
 }
 
 dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("com.querydsl:querydsl-jpa:4.4.0")
 
     implementation("org.springframework.boot:spring-boot-starter")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
@@ -34,7 +34,48 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.retrofit2:adapter-java8:2.9.0")
 
+    /* GRPC */
+    implementation("io.grpc:grpc-kotlin-stub:1.3.0")
+    implementation("com.google.protobuf:protobuf-java:3.24.3")
+    implementation("io.grpc:grpc-netty-shaded:1.58.0")
+    implementation("io.grpc:grpc-protobuf:1.58.0")
+    implementation("net.devh:grpc-server-spring-boot-starter:2.15.0.RELEASE")
+
     implementation(project(":util"))
     implementation(project(":domain"))
     implementation(project(":application"))
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.24.3"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.58.0"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:1.4.0:jdk8@jar"
+        }
+    }
+    generateProtoTasks {
+        ofSourceSet("main").forEach {
+            it.plugins {
+                add(GenerateProtoTask.PluginOptions("grpc"))
+                add(GenerateProtoTask.PluginOptions("grpckt"))
+            }
+        }
+    }
+}
+
+sourceSets {
+    main {
+        proto {
+            srcDir("content/adapter/src/main/proto")
+        }
+        java {
+            srcDirs("content/adapter/build/generated/source/proto/main/grpc")
+            srcDirs("content/adapter/build/generated/source/proto/main/java")
+        }
+    }
 }
