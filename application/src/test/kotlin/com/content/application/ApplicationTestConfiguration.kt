1d2def9c30831f230c98a87f5362911ce031c27c
gosekose <88478829+gosekose@users.noreply.github.com>
2023-10-15 17:35:32 +0900

    [Fix] DPLC-031: 빌드 의존성 문제 해결 (#30)

--- application/src/test/kotlin/com/content/application/ApplicationTestConfiguration.kt
+++ application/src/test/kotlin/com/content/application/ApplicationTestConfiguration.kt
@@ -1,10 +1,12 @@
 package com.content.application
 
-import org.springframework.boot.SpringBootConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
-@SpringBootConfiguration
-@SpringBootApplication
+@EnableJpaRepositories(basePackages = ["com.content.adapter.**"])
+@EntityScan(basePackages = ["com.content.adapter.**"])
 @ComponentScan(basePackages = ["com.content.**"])
+@SpringBootApplication
 class ApplicationTestConfiguration
