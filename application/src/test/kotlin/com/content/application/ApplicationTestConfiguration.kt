2669180b32e1163d66769dc277dd44810d264f0c
gosekose <ksyn1611@naver.com>
2023-10-17 00:17:02 +0900

    [Feat] DPLC-036: 리뷰 그룹 생성을 위한 의존성 설정

--- application/src/test/kotlin/com/content/application/ApplicationTestConfiguration.kt
+++ application/src/test/kotlin/com/content/application/ApplicationTestConfiguration.kt
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
+@SpringBootApplication(scanBasePackages = ["com.content.**"])
 @EnableJpaRepositories(basePackages = ["com.content.adapter.**"])
-@EntityScan(basePackages = ["com.content.adapter.**"])
+@EntityScan(basePackages = ["com.content.**"])
 @ComponentScan(basePackages = ["com.content.**"])
-@SpringBootApplication
 class ApplicationTestConfiguration
