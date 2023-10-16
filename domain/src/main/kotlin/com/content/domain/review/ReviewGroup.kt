f2f8f24381a44ce512c41f018fe7c3ea717dcab8
gosekose <ksyn1611@naver.com>
2023-10-17 00:18:41 +0900

    [Feat] DPLC-036: 리뷰 그룹 생성 도메인

--- domain/src/main/kotlin/com/content/domain/review/ReviewGroup.kt
+++ domain/src/main/kotlin/com/content/domain/review/ReviewGroup.kt
@@ -1,6 +1,16 @@
 package com.content.domain.review
 
+import java.time.LocalDateTime
+
 data class ReviewGroup(
     val courseGroupId: Long,
     val reviewGroupId: Long,
-)
+    val reviewGroupName: String,
+    val userId: Long,
+    val createdAt: LocalDateTime,
+    val modifiedAt: LocalDateTime,
+) {
+    companion object {
+        const val DEFAULT_NAME = "제목없음"
+    }
+}
