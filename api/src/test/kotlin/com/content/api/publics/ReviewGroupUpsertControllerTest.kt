f9762fa8600bc521642cfba4e45cb18cf42d9a7d
gosekose <ksyn1611@naver.com>
2023-10-17 03:29:29 +0900

    [Test] DPLC-040: 리뷰 그룹 생성 및 업데이트 테스트

--- api/src/test/kotlin/com/content/api/publics/ReviewGroupUpsertControllerTest.kt
+++ api/src/test/kotlin/com/content/api/publics/ReviewGroupUpsertControllerTest.kt
@@ -20,7 +20,6 @@ import io.mockk.just
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.http.MediaType
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
@@ -50,11 +49,8 @@ class ReviewGroupUpsertControllerTest : FunSpec() {
     @MockkBean
     private lateinit var reviewGroupCommandUseCase: ReviewGroupCommandUseCase
 
-    @LocalServerPort
-    private val port: Int = 0
-
     init {
-        this.context("mockk 스텁 결과가 주어져요") {
+        this.context("ReviewGroup 생성에 대한 mockk 스텁 결과가 주어져요") {
             val userId = 1L
 
             every { userVerifyService.verifyNormalUserAndGet(any()) } returns User(
@@ -98,12 +94,58 @@ class ReviewGroupUpsertControllerTest : FunSpec() {
             }
 
             test("잘못된 json 요청으로 리뷰 그룹 생성 응답을 검증을 실패해요") {
-                val reviewCreateGroupApiRequest = "{\"courseGroupId2\": 12345}"
+                val request = "{\"courseGroupId2\": 12345}"
 
                 val mockHttpServletRequestBuilder = MockMvcRequestBuilders.post("https://localhost:8078/content/reviewgroups")
                     .contentType(MediaType.APPLICATION_JSON)
                     .header("UserId", userId)
-                    .content(reviewCreateGroupApiRequest)
+                    .content(request)
+
+                mockMvc.perform(mockHttpServletRequestBuilder)
+                    .andExpect(status().is4xxClientError)
+            }
+        }
+
+        this.context("ReviewGroup 업데이트에 대한 mockk 스텁 결과가 주어져요") {
+            val userId = 1L
+
+            every { userVerifyService.verifyNormalUserAndGet(any()) } returns User(
+                userId = userId,
+                userAccountStatus = UserAccountStatus.NORMAL,
+                nickName = "UserA"
+            )
+
+            every { reviewGroupCommandUseCase.updateReviewGroup(any(), any(), any()) } returns ReviewGroup(
+                courseGroupId = userId,
+                reviewGroupId = 1L,
+                reviewGroupName = "ReviewA",
+                userId = 1L,
+                createdAt = LocalDateTime.now(),
+                modifiedAt = LocalDateTime.now(),
+            )
+
+            test("올바른 json 요청으로 리뷰 그룹 생성 응답을 검증을 성공해요") {
+                val request = "{\"reviewGroupName\": \"ReviewA\"}"
+
+                val mockHttpServletRequestBuilder = MockMvcRequestBuilders.put("https://localhost:8078/content/reviewgroups/1")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .header("UserId", userId)
+                    .content(request)
+
+                mockMvc.perform(mockHttpServletRequestBuilder)
+                    .andExpect(status().isOk)
+                    .andExpect(
+                        jsonPath("$.reviewGroupId").value("1")
+                    )
+            }
+
+            test("잘못된 json 요청으로 리뷰 그룹 생성 응답을 검증을 실패해요") {
+                val request = "{\"reviewGroupName2\": \"ReviewA\"}"
+
+                val mockHttpServletRequestBuilder = MockMvcRequestBuilders.put("https://localhost:8078/content/reviewgroups/1")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .header("UserId", userId)
+                    .content(request)
 
                 mockMvc.perform(mockHttpServletRequestBuilder)
                     .andExpect(status().is4xxClientError)
