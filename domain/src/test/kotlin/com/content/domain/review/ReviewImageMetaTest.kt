710ae8fa986483d90bfb039376371c96a403a2b1
gosekose <88478829+gosekose@users.noreply.github.com>
2023-10-19 01:49:30 +0900

    [Fix] DPLC-060: ReviewImageMeta 수정 (#62)

---
+++ domain/src/test/kotlin/com/content/domain/review/ReviewImageMetaTest.kt
@@ -0,0 +1,24 @@
+package com.content.domain.review
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class ReviewImageMetaTest : FunSpec({
+
+    context("ReviewImageMeta가 주어져요") {
+        val reviewImageMeta = ReviewImageMeta(
+            sequence = 2,
+            reviewId = 1L,
+            originalName = "image_124.png",
+            reviewImageHashCode = "Hello".hashCode(),
+            reviewImageId = 0L,
+        )
+
+        test("ReviewImageMeta를 검증해요") {
+            println(reviewImageMeta.imageName)
+            reviewImageMeta.imageUrl shouldBe "/${reviewImageMeta.reviewId}/${reviewImageMeta.sequence}/${reviewImageMeta.imageName}"
+            reviewImageMeta.imageName.split(".").size shouldBe 2
+        }
+    }
+
+})
